74a668c29c7ddaf7ac8d3535f3cb66924695cc1c
kgadgil <kgadgil@github.com>
2019-01-12 22:41:18 -0500

    possible encap of node in class block

--- ebr.cpp
+++ ebr.cpp
@@ -4,57 +4,72 @@
 *****************/
 #include<iostream>
 #include<vector>
+#include <thread>
 
 template<typename T>
-int64 epoch;
-int64 reservations[thread_cnt];
-therad_local int counter;
-thread_local std::vector<int> retired;
-int epoch_freq=1; // freq. of increasing epoch
-int empty_freq=1; // freq. of reclaiming retired
-
 class block {
-	//block header : retire_epoch, retire_next
-	// Node(eg: list node)
-	double data;
-	block* next;
-	T retire_epoch;
+		//block header : retire_epoch, retire_next
+		// Node(eg: list node)
+	int retire_epoch;
+	struct Node {
+		T data;
+		Node* next;
+	}
 }
 
-//Start of an operation
-void start_op(){
-	reservations[thread_id()] = epoch;
-}
-
-//End of an operation
-void enq_op(){
-	reservations[thread_id()] = MAX_INT64;
-}
-
-//public interface
-//block will not be used by any future operation
-//?? paper says (block*ptr) maybe lecture said (block&ptr)
-void retire (block & ptr){
-	retired.append(ptr);
-	ptr.retire_epoch = epoch;
-	counter++;
-	if (counter % epoch_freq == 0)
-		fetch_and_add(epoch,1);
-	if (retired.cnt % empty_freq == 0)
-		empty();
-}
-
-//private function
-void empty(){
-	max_safe_epoch = reservations.min();
-	for block belongs to retired
-	//all blocks retired IN or AFTER max_save_epoch will be protected	
-		if (block.retire_epoch < max_safe_epoch){
+template<typename T>
+class memory_manager(){
+	int64 epoch;
+	int64 reservations;
+	thread_local int counter;
+	thread_local std::vector<int> retired;
+	int epoch_freq; // freq. of increasing epoch
+	int empty_freq; // freq. of reclaiming retired
+private:
+	void empty(){
+		//max_safe_epoch = reservations.min();
+		std::vector<int>::iterator max_safe_epoch = std::min_element(std::begin(retired), std::end(retired));
+		for(auto block : retired){
+				//all blocks retired IN or AFTER max_save_epoch will be protected	
+			if (block.retire_epoch < max_safe_epoch){
 			//deallocate block
-			free(block)
+				free(block)
+			}
 		}
-}
 
+	}
+
+public:
+	memory_manager(int thread_cnt){
+		epoch=0;
+		reservations[thread_cnt];
+		counter = 0;
+		epoch_freq = 1;
+		empty_freq = 1;
+	}
+
+	void start_op(){
+		//Start of an operation
+		reservations[std::this_thread::get_id()] = epoch;
+	}
+
+	void enq_op(){
+		//End of an operation
+		reservations[std::this_thread::get_id()] = MAX_INT64;
+	}
+
+	void retire (block & ptr){
+		//block will not be used by any future operation
+		//?? paper says (block*ptr) maybe lecture said (block&ptr)
+		retired.push_back(ptr);
+		ptr.retire_epoch = epoch;
+		counter++;
+		if (counter % epoch_freq == 0)
+			fetch_and_add(epoch,1);
+		if (retired.cnt % empty_freq == 0)
+			empty();
+	}
+}
 
 /*linked list;s remove method
 remove()
@@ -74,42 +89,46 @@ search () {
 therefore we need efficient implementation for start_op and end_op
 */
 
-// set-ups
-memory_manager<node> mm;
-// 3 nodes with values 0, 2 and 4
-//write node class 
-node n0(0), n1(2), n2(4);
-n0.next = &n1;
-n1.next = &n2;
-n2.next = NULL;
-node* head = &n;
+int main() {
+	block blk;
+	// set-ups
+	memory_manager<Node> mm;
+	// 3 nodes with values 0, 2 and 4
+	node n0(0), n1(2), n2(4);
+	n0.next = &n1;
+	n1.next = &n2;
+	n2.next = NULL;
+	node* head = &n;
 
-// 2 worker threads
-int thread_A(){
-	// Thread A tries to read n1
-	mm.start_op();
-	node* target = &n1;
-	node* p1 = mm.read(&target);
-	if (p1 != NULL){ // n1 still exists
-		int ret = p1->val; mm.end_op();
-		return ret;
-	}
-	else {
-		mm.end_op();
-		return NULL;
-	}
-}
-void thread_B(){
-	// Thread B tries to update n1
-	while(true){
+	std::thread tA([&]{
+    	// Thread A tries to read n1
 		mm.start_op();
-		node* new_n1 = mm.alloc(sizeof(node));
-		new_n1.val = 3;
-		new_n1.next = &n2;
-		node* target = &n0;
-		node* p0 = mm.read(&target);
-		if (p0 != NULL){
-			if (mm.compare_exchange_weak(&p0->next, &n1,std::memory_order_release, std::memory_order_relaxed))
+		node* target = &n1;
+		node* p1 = mm.read(&target);
+		if (p1 != NULL){ // n1 still exists
+			int ret = p1->val; mm.end_op();
+			return ret;
 		}
-	}
+		else {
+			mm.end_op();
+			return NULL;
+		}
+	});
+
+	std::thread tB([&]{
+		// Thread B tries to update n1
+		while(true){
+			mm.start_op();
+			node* new_n1 = mm.alloc(sizeof(node));
+			new_n1.val = 3;
+			new_n1.next = &n2;
+			node* target = &n0;
+			node* p0 = mm.read(&target);
+			if (p0 != NULL){
+				if (mm.compare_exchange_weak(&p0->next, &n1,std::memory_order_release, std::memory_order_relaxed))
+			}
+		}
+	});
+
+	return 0;	
 }
