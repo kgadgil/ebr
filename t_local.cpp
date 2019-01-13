e5a98e5a15068d07f5779c598c347bebdda8aa48
kgadgil <kgadgil@github.com>
2019-01-13 02:01:27 -0500

    int counter per thread without thread_local

--- t_local.cpp
+++ t_local.cpp
@@ -4,22 +4,20 @@ Thread-local storage class specifier example
 #include <iostream>
 #include <thread>
 
-void function(int cnt){
-	//thread_local int cnt=0;
-	cnt++;
-	std::cout << "cnt " << cnt << std::endl;
-}
-
 int main(){
 	int thread_cnt = 6;
-	int start=0;
+	int x = 0;
 	std::thread threadArr[thread_cnt];
 	for (int i = 0; i < thread_cnt; i++){
-		threadArr[i] = std::thread(function, start);
+		threadArr[i] = std::thread([&]{
+			x++;
+			std::cout << "thread id "<< std::this_thread::get_id() << std::endl;
+			std::cout << "inc " << x << std::endl;
+		});
 	}
 	for (int i = 0; i < thread_cnt; i++){
 		threadArr[i].join();
 	}
-	std::cout << "value " << start << std::endl;
+	std::cout << "outside " << x << std::endl;
 	return 0;
 }
