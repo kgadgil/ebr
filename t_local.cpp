5a6a20fc86ebcaf00c4994940a8f621001ea20b5
kgadgil <kgadgil@github.com>
2019-01-13 00:05:27 -0500

    each thread increments on its own, but this is local to the function not the thread

--- t_local.cpp
+++ t_local.cpp
@@ -4,20 +4,22 @@ Thread-local storage class specifier example
 #include <iostream>
 #include <thread>
 
-void function(){
-	thread_local int cnt=0;
+void function(int cnt){
+	//thread_local int cnt=0;
 	cnt++;
 	std::cout << "cnt " << cnt << std::endl;
 }
 
 int main(){
 	int thread_cnt = 6;
+	int start=0;
 	std::thread threadArr[thread_cnt];
 	for (int i = 0; i < thread_cnt; i++){
-		threadArr[i] = std::thread(function);
+		threadArr[i] = std::thread(function, start);
 	}
 	for (int i = 0; i < thread_cnt; i++){
 		threadArr[i].join();
 	}
+	std::cout << "value " << start << std::endl;
 	return 0;
 }
